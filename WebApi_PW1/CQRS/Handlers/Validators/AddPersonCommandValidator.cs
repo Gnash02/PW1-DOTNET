2a13d8a9952689640704307dcfc013b88ede3085
gn02ash <guezrihiba7@gmail.com>
2024-02-05 13:57:02 +0100

    more conditions

--- WebApi_PW1/CQRS/Handlers/Validators/AddPersonCommandValidator.cs
+++ WebApi_PW1/CQRS/Handlers/Validators/AddPersonCommandValidator.cs
@@ -9,6 +9,8 @@ public class AddPersonCommandValidator:AbstractValidator<AddPersonCommand>
    public AddPersonCommandValidator()
    {
       RuleFor(x => x.Name).NotEmpty().WithMessage("the person's name cannot be empty");
+      RuleFor(x => x.Name).Matches(@"^[a-zA-Z]+$").WithMessage("Name must be  valid string ");   
+      RuleFor(x => x.Age).GreaterThan(15).WithMessage("The age must be greater than 15 yo");
     //  RuleFor(x => x.Name).NotEmpty().WithMessage("the person's name cannot be empty");
 
       RuleFor(x => x.Age).NotEmpty().WithMessage("the age cannot be empty");
